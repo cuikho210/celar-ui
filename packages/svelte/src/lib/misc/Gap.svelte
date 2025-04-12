797bd7160b2c6ad77091f48342f0f72e45b8012f
cuikho210 <tonykms210@gmail.com>
2025-04-12 23:14:46 +0700

    feat: add duck spinner and gap components

---
+++ packages/svelte/src/lib/misc/Gap.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	interface Props {
+		size?: string;
+	}
+
+	let { size }: Props = $props();
+</script>
+
+<div data-gap style:width={size}></div>
+
+<style lang="scss">
+	[data-gap] {
+		display: block;
+		aspect-ratio: 1;
+		width: 1rem;
+		height: auto;
+	}
+</style>
