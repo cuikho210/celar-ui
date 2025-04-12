76ef4d0b18c92611eeb20b4b39b776c4f3646067
cuikho210 <tonykms210@gmail.com>
2025-04-12 17:09:17 +0700

    feat: create styles directory
    Sets up basic colors, spacing, and misc styles with scss
    variables. Adds a base layout.

---
+++ packages/svelte/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '$style/spacing.scss';
+	import '$style/colors.scss';
+	import '$style/misc.scss';
+	import './style.scss';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
