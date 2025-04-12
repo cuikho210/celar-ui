d7576b0daff51a7832f51c6a2a3a6654ab1ddb9f
cuikho210 <tonykms210@gmail.com>
2025-04-12 16:31:32 +0700

    feat: add sass and autoprefixer
    Adds sass and autoprefixer as dependencies. Also adds a postcss
    config to the vite config.

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [autoprefixer({})]
+		}
+	},
 	server: {
 		allowedHosts: true
 	}
