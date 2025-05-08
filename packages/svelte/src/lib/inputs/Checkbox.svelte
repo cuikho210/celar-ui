687709675ca26f32f311e169da924c99d72861f6
cuikho210 <tonykms210@gmail.com>
2025-05-09 01:04:34 +0700

    feat: replace iconify icons with inline svgs in checkbox
    The iconify icons have been replaced with inline svgs in the
    checkbox component. Also bumped package version to 0.0.8.

--- packages/svelte/src/lib/inputs/Checkbox.svelte
+++ packages/svelte/src/lib/inputs/Checkbox.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { Checkbox as BitCheckbox, type CheckboxRootProps, type WithoutChildren } from 'bits-ui';
-	import IconTick from '~icons/hugeicons/tick-01';
-	import IconMinus from '~icons/hugeicons/minus-sign';
 	import type { Snippet } from 'svelte';
 
 	type CheckboxProps = WithoutChildren<CheckboxRootProps> & {
@@ -11,15 +9,43 @@
 	let { children, ...rest }: CheckboxProps = $props();
 </script>
 
+{#snippet iconTick()}
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M5 14.5s1.5 0 3.5 3.5c0 0 5.559-9.167 10.5-11"
+			color="currentColor"
+		/>
+	</svg>
+{/snippet}
+
+{#snippet iconMinus()}
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M20 12H4"
+			color="currentColor"
+		/>
+	</svg>
+{/snippet}
+
 <label data-checkbox>
 	<div data-checkbox-icon>
 		<BitCheckbox.Root {...rest}>
 			{#snippet children({ checked, indeterminate })}
 				{#if checked}
-					<IconTick font-size="20px" />
+					{@render iconTick()}
 				{/if}
 				{#if indeterminate}
-					<IconMinus />
+					{@render iconMinus()}
 				{/if}
 			{/snippet}
 		</BitCheckbox.Root>
