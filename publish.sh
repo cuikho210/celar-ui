f5248bde671d5be1fc04884fe1004f2f0f5a6862
cuikho210 <tonykms210@gmail.com>
2025-05-25 16:59:34 +0700

    move publish script to package.json and bump version

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-bun run build
-
-npm publish --access=public
--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ cd "$(dirname "$0")"
 
 export $(cat ".env" | xargs)
 alias cm=$PWD/commit.sh
-alias publish=$PWD/publish.sh
 
 cd -
